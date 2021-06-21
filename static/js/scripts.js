e9be62d18a9869d3a521cf54b2e9369ecc6eb339
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-06-21 11:37:00 +0300

    adds a function t0 show description on click

---
+++ static/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $("#imagedesign").click(function(){
+       $("#design").show()
+    //    $("#design").fadeOut()
+   })
+     $("#imagedev").click(function(){
+       $("#dev").show()
+   })
+   $("#imageproduct").click(function(){
+    $("#product").show()
+})
+})
