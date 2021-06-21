f812eafeaa804a07f9a6b3bce47c9c44c076bb37
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-06-21 16:04:48 +0300

    design portfolio section

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -12,55 +12,16 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $("#work1").mouseover(function(){
-        $("#overlay").show();
+    $(".images").animate({
+        opacity: 1
     });
-    $("#work1").mouseout(function(){
-        $("#overlay").hide()
-    })
-    $("#work2").mouseover(function(){
-        $("#overlay1").show();
+    $(".images").hover(function(){
+        $(this).stop().animate({opacity: .4},200);
+    }, function(){
+        $(this).stop().animate({opacity:1},500)
     });
-    $("#work2").mouseout(function(){
-        $("#overlay1").hide()
-    })
-    $("#work3").mouseover(function(){
-        $("#overlay2").show();
-    });
-    $("#work3").mouseout(function(){
-        $("#overlay2").hide()
-    })
-    $("#work4").mouseover(function(){
-        $("#overlay3").show();
-    });
-    $("#work4").mouseout(function(){
-        $("#overlay3").hide()
-    })
-    $("#work5").mouseover(function(){
-        $("#overlay4").show();
-    });
-    $("#work5").mouseout(function(){
-        $("#overlay4").hide()
-    })
-    $("#work6").mouseover(function(){
-        $("#overlay5").show();
-    });
-    $("#work6").mouseout(function(){
-        $("#overlay5").hide()
-    })
-    $("#work7").mouseover(function(){
-        $("#overlay6").show();
-    });
-    $("#work7").mouseout(function(){
-        $("#overlay6").hide()
-    })
-    $("#work8").mouseover(function(){
-        $("#overlay7").show();
-    });
-    $("#work8").mouseout(function(){
-        $("#overlay7").hide()
-    })
-})
+    $()
+});
 
 $(document).ready(function(){
     $("form#theform").submit(function(event){
